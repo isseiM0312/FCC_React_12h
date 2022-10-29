60cf0bc4f7c44e04dc76057cbe638860ec5387e2
issei <isseieikisouya@outlook.jp>
2022-10-29 14:05:57 +0900

    コンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
   return (
     <div>
       <header>
-        <nav>
-        <img src={logo} width = "400px"></img>
+        <nav className='nav'>
+        <img className='nav-logo' src={logo}></img>
+        <ul className='nav-item'>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
         </nav>
-      </header>
+    </header>
+    </div>
+  )
+}
+
+function MainContent() {
+  return(
+    <div>
        <h1>Reasons I'm excited to learn React</h1>
        <ol>
         <li>It's a popular library, so I'l be able to fit in with the cool kids!</li>
         <li>I'm more likely to get a job as a developer if I know React</li>
        </ol>
-       <footer>
+    </div>
+  )
+}
+
+function Footer() {
+  return(
+    <div>
+      <footer>
         <small>© 2021 Issei dev.</small> 
        </footer>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+       <Footer />
     </div>
    
   );
@@ -26,3 +55,4 @@ export default App;
 
 //navはページ内リンク
 //olは番号の箇条書き
+//関数は<App / >のようにhmtlタグでやるとrenderされる
